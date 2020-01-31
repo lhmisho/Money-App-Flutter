c38aa0d0ada7cf6e88c5792cf0c3216bbf2d386a
lhmisho <lhmisho@gmail.com>
2020-01-31 18:21:03 +0600

    Basic setup

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('List of TX'),
+          ),
+        ],
+      ),
+    );
+  }
+}
